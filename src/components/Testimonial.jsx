233afb33d89007b7051924328cd14f420d6b5ef9
Raiyan109 <kraiyan109@gmail.com>
2024-07-04 10:48:22 +0600

    slick slider

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,3 +1,7 @@
+import Slider from 'react-slick'
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import avatar1 from '../assets/Ellipse 10(1).png'
 import avatar2 from '../assets/Ellipse 10(2).png'
 import avatar3 from '../assets/Ellipse 10.png'
@@ -26,6 +30,16 @@ const data = [
     },
 ]
 const Testimonial = () => {
+    const settings = {
+        dots: true,
+        arrows: false,
+        dotsClass: "slick-dots slick-thumb",
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    };
+
     return (
         <div className="py-20 space-y-5">
             <div className="">
@@ -34,22 +48,27 @@ const Testimonial = () => {
                 </div>
                 <h1 className='section-headings'>What they say about us</h1>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                {data.map((d) => (
-                    <div key={d.name} className="bg-offWhite p-5 flex flex-col gap-5 rounded-3xl">
-                        <h1 className='text-2xl font-semibold text-primary max-w-sm'>{d.heading}</h1>
-                        <p className='text-sm text-primary font-light max-w-sm'>{d.paragraph}</p>
-                        <div className='flex items-center gap-3'>
-                            <img src={d.avatar} alt="" />
-                            <div className='flex flex-col gap-1'>
-                                <h1 className='text-sm'><strong className='text-primary'>{d.name}</strong>, {d.title}</h1>
-                                <div>
-                                    <img src={star} alt="" />
+            {/* grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 */}
+            <div className="">
+                <div className='mt-10'>
+                    <Slider {...settings}>
+                        {data.map((d) => (
+                            <div key={d.name} className="bg-offWhite p-5 flex flex-col gap-5 rounded-3xl space-y-2 h-56">
+                                <h1 className='text-2xl font-semibold text-primary max-w-sm leading-tight'>{d.heading}</h1>
+                                <p className='text-sm text-primary font-light max-w-sm'>{d.paragraph}</p>
+                                <div className='flex items-center gap-3'>
+                                    <img src={d.avatar} alt="" />
+                                    <div className='flex flex-col gap-1'>
+                                        <h1 className='text-sm'><strong className='text-primary'>{d.name}</strong>, {d.title}</h1>
+                                        <div>
+                                            <img src={star} alt="" />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                ))}
+                        ))}
+                    </Slider>
+                </div>
             </div>
         </div>
     );
